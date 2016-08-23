eeb1b16b7943b19e3f2fc091d4e81308d4f0be57
Eric Tsai <eric.tsai@tenmax.io>
2016-08-23 22:41:07 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,127 @@
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import colors from 'colors';
+import http from 'http';
+import { GraphQLObjectType, GraphQLString, GraphQLSchema } from 'graphql';
+import graphqlHttp from 'express-graphql';
+
+import routes from 'routes/index';
+
+var data = require('./data.json');
+
+var userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }
+});
+
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      user: {
+        type: userType,
+        // `args` describes the arguments that the `user` query accepts
+        args: {
+          id: { type: GraphQLString }
+        },
+        // The resolve function describes how to "resolve" or fulfill
+        // the incoming query.
+        // In this case we use the `id` argument from above as a key
+        // to get the User from `data`
+        resolve: function (_, args) {
+          return data[args.id];
+        }
+      }
+    }
+  })
+});
+
+
+
+const port = process.env.PORT || '3000';
+const app = express();
+
+app.set('port', port);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// app.use('/', routes);
+app.use('/graphql', graphqlHttp({
+  schema,
+  pretty: true
+}));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError (error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+  case 'EACCES':
+    console.error(bind + ' requires elevated privileges');
+    process.exit(1);
+    break;
+  case 'EADDRINUSE':
+    console.error(bind + ' is already in use');
+    process.exit(1);
+    break;
+  default:
+    throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log(`Listening on ${bind}`.green);
+}
