7ed5d07be9669fe3a95fbd2416103461766bf177
Eric Tsai <eric.tsai@tenmax.io>
2016-09-12 23:26:45 +0800

    add view engine

--- server.js
+++ server.js
@@ -1,88 +1,36 @@
-import express from 'express';
-import path from 'path';
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import colors from 'colors';
-import http from 'http';
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-session';
+import passport from 'koa-passport';
+import convert from 'koa-convert';
+import views from 'koa-views';
 
-import routes from './routes/index';
-
+const app = new Koa();
 const port = process.env.PORT || '3000';
-const app = express();
+const env = process.env.NODE_ENV || 'development';
 
-app.set('port', port);
+app.use(bodyParser());
 
-app.use(express.static('public'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.keys = ['secret'];
+app.use(convert(session(app)));
 
-app.use('/', routes);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(views(`${__dirname}/views`, {
+  map: { hbs: 'handlebars' }
+}));
+
+app.use(async ctx => {
+  await ctx.render('index.hbs');
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(async (ctx, next) => {
+  ctx.body = 'hello';
 });
 
-const server = http.createServer(app);
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-function onError (error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-  case 'EACCES':
-    console.error(bind + ' requires elevated privileges');
-    process.exit(1);
-    break;
-  case 'EADDRINUSE':
-    console.error(bind + ' is already in use');
-    process.exit(1);
-    break;
-  default:
-    throw error;
-  }
-}
+app.listen(port, onListening);
 
 function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  console.log(`Listening on ${bind}`.green);
+  console.log(`Listening on ${port}`);
 }
