072ffc61943b45c8030e8d4210ab42d6583f966c
ericyan <eric7578@gmail.com>
2017-03-21 23:51:29 +0800

    remove adapters, bin, commands

--- adapters/permissionSamples.js
+++
@@ -1,9 +0,0 @@
-import { set } from '../utils/bitop';
-
-export const ACCESS = 0;
-export const MODIFY = 1;
-export const REMOVE = 2;
-
-export const READER = [ACCESS];
-export const EDITOR = [ACCESS, MODIFY];
-export const OWNER = [ACCESS, MODIFY, REMOVE];
